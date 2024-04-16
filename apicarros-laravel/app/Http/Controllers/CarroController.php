53837ab4a13ccb2631d7e682fdf60c9b0543f402
Bernardo Dartora <bernardintd@gmail.com>
2024-04-16 14:58:48 -0300

    new-api-carros-laravel

--- apicarros-laravel/app/Http/Controllers/CarroController.php
+++ apicarros-laravel/app/Http/Controllers/CarroController.php
@@ -3,8 +3,86 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class CarroController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $carros = Cache::get('carros');
+
+        return response()->json(['carros' => json_decode($carros) ]);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+    if(!$request->has(['id','modelo', 'marca'])){
+        return response()->json(['error' => "Campos obrigatórios ausentes"]);
+      }
+
+     $carros = json_decode(Cache::get('carros'));
+
+     $carro = $request->json()->all();
+     $carros[] = $carro;
+
+
+     Cache::put('carros', json_encode($carros));
+
+      return response()->json(['success' => true, 'mgs' => "Carro adicionado com sucesso!"]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $carros = json_decode(Cache::get('carros'), true);
+
+
+    foreach($carros as $carro){
+        if($carro['id']  == $id){
+            return response()->json(['carro' => $carro]);
+        }
+    }
+
+
+    return response()->json(['error' => 'carro não encontrado!' ]);
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $carros = json_decode(Cache::get('carros'), true);
+
+
+    foreach ($carros as $key => $carro) {
+        if ($carro['id'] == $id) {
+            array_splice($carros, $key, 1);
+            Cache::put('carros', json_encode($carros));
+
+        }
+    }
+    return response()->json(['success' => true, 'mgs' => "Carro excluido!"]);
+    }
 }
--- apicarros-laravel/routes/api.php
+++ apicarros-laravel/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +17,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+// Route::post('/carros', function(Request $request){
+
+
+//     if(!$request->has(['id','modelo', 'marca'])){
+//       return response()->json(['error' => "Campos obrigatórios ausentes"]);
+//     }
+
+//    $carros = json_decode(Cache::get('carros'));
+
+//    $carro = $request->json()->all();
+//    $carros[] = $carro;
+
+
+//    Cache::put('carros', json_encode($carros));
+
+//     return response()->json(['success' => true, 'mgs' => "Carro adicionado com sucesso!"]);
+
+//  });
+
+
+
+
+// Route::get('/carros/get', function(){
+
+//     $carros = Cache::get('carros');
+
+//     return response()->json(['carros' => json_decode($carros) ]);
+
+// });
+
+
+// Route::get('/carros/{id}', function($id){
+
+//     $carros = json_decode(Cache::get('carros'), true);
+
+
+//     foreach($carros as $carro){
+//         if($carro['id']  == $id){
+//             return response()->json(['carro' => $carro]);
+//         }
+//     }
+
+
+//     return response()->json(['error' => 'carro não encontrado!' ]);
+
+// });
+
+
+
+
+// Route::delete('/carros/{id}', function($id){
+
+//     $carros = json_decode(Cache::get('carros'), true);
+
+
+//     foreach ($carros as $key => $carro) {
+//         if ($carro['id'] == $id) {
+//             array_splice($carros, $key, 1);
+//             Cache::put('carros', json_encode($carros));
+
+//         }
+//     }
+//     return response()->json(['success' => true, 'mgs' => "Carro excluido!"]);
+
+// });
+
+Route::resource('/carros', CarroController::class);
