e196038463c212400e74803daecb2918a984d79b
Bernardo Dartora <bernardintd@gmail.com>
2024-04-29 16:04:09 -0300

    dados que serao maniplados na criação do produto

--- start-laravel/app/Models/Product.php
+++ start-laravel/app/Models/Product.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//use HasFactory; //quando quero criar um criador de produtos
 class Product extends Model
 {
-    use HasFactory; //quando quero criar um criador de produtos
+    protected $fillable = [ //esses sao os campos que vao poder ser malipulados na criação do PRODUTO
+        'name',
+        'price',
+        'description',
+        'enabled'
+    ];
 }
