d9d01357532992a82187a52e1ec17a12e209b288
Bernardo Dartora <bernardintd@gmail.com>
2024-04-29 15:56:52 -0300

    make a model

---
+++ start-laravel/app/Models/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory; //quando quero criar um criador de produtos
+}
