4dd4a07de9a27fb79b3134e6392ea2f7c1b6f2fa
Bernardo Dartora <bernardintd@gmail.com>
2024-05-02 16:31:36 -0300

    implementacao do crud

--- api-biblioteca/app/Http/Controllers/LivrosController.php
+++ api-biblioteca/app/Http/Controllers/LivrosController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\livro;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class LivrosController extends Controller
@@ -9,9 +11,21 @@ class LivrosController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        try{
+            if ($request->has('nome')) {
+                $livro = livro::where('nome', $request->nome)->get();
+                return response()->json(['nome' => $livro]);
+            }
+
+            $livros = livro::all();
+
+            return response()->json(['nome' => $livros]);
+
+        }catch(ModelNotFoundException $error){
+            return response()->json(['success' => false, 'msg' => $error->getMessage()], 404);
+        }
     }
 
     /**
@@ -19,7 +33,24 @@ class LivrosController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'nome' => 'required',
+                'autor' => 'required',
+                'paginas' => 'required'
+            ],
+            [
+                'nome.required' => 'Campo titulo obrigatorio',
+                'autor.required' => 'Campo autor obrigatorio',
+                'paginas.required' => 'Campo numero de paginas obrigatorio'
+            ]);
+
+            $livro = livro::create($request->all());
+
+            return response()->json(['success' => true, 'msg' => "Carro criado com sucesso.", 'data' => $livro]);
+        } catch (\Exception $error) {
+            return response()->json(['success' => false, 'msg' => $error->getMessage()], 404);
+        }
     }
 
     /**
@@ -27,7 +58,12 @@ class LivrosController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try{
+            $livro = livro::findOrFail($id);
+            return response()->json(['success' => true, 'msg' => "Listado carro.", 'data' => $livro ]);
+        }catch(ModelNotFoundException $error){
+            return response()->json(['success' => false, 'msg' => $error->getMessage()], 404);
+        }
     }
 
     /**
@@ -35,7 +71,36 @@ class LivrosController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $request->validate([
+                'nome' => 'required',
+                'autor' => 'required',
+                'paginas' => 'required'
+            ],
+            [
+                'nome.required' => 'Campo nome obrigatorio',
+                'autor.required' => 'Campo autor obrigatorio',
+                'paginas.required' => 'Campo numero de paginas obrigatorio'
+            ]);
+
+            $livro = livro::findOrFail($id);
+
+
+
+            $livro->fill([
+                'nome' => $request->nome,
+                'autor' => $request->autor,
+                'paginas' => $request->paginas
+
+            ]);
+            //save() para salvar a alteracao no banco de dados
+            $livro->save();
+
+            return response()->json(['success' => true, 'mgs' => "Livro editado!", 'data' => $livro]);
+
+        } catch (ModelNotFoundException $error) {
+            return response()->json(['success' => false, 'msg' => $error->getMessage()], 400);
+        }
     }
 
     /**
@@ -43,6 +108,14 @@ class LivrosController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $livro = livro::findOrFail($id);
+            $livro->delete();
+
+            return response()->json(['success' => true, 'mgs' => 'Livro deletado da biblioteca com sucesso', 'data' => $livro]);
+
+        }catch(ModelNotFoundException $error){
+            return response()->json(['success' => false, 'msg' => $error->getMessage()], 404);
+        }
     }
 }
