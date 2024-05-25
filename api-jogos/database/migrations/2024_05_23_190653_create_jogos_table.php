b431b36114efbffc4712a51a055fa7cf7f616f35
Bernardo Dartora <bernardintd@gmail.com>
2024-05-25 11:53:14 -0300

    migration da api jogos para o banco

---
+++ api-jogos/database/migrations/2024_05_23_190653_create_jogos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jogos', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name');
+            $table->string('storage');
+            $table->integer('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jogos');
+    }
+};
