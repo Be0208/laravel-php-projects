3aaeebcccd4c69cec60ae18910777712b247ea3f
Bernardo Dartora <bernardintd@gmail.com>
2024-05-29 17:11:44 -0300

    parte de autenticação

--- dinamica-api/routes/api.php
+++ dinamica-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+// Route::resource('/User', UserController::class);
+Route::resource('/login', AuthController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function (){
+    // Route::resource('/post', PostController::class);
+
 });
